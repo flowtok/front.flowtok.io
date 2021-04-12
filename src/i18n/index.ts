586acad4db57610dec81d6e4c95dd61b5c5729a3
YoungProducer <sashabezrukovownmail@gmail.com>
2021-04-12 17:49:25 +0300

    Added i18n and ru translations.

---
+++ src/i18n/index.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationRU from '../translations/ru/translation.json';
+
+const resources = {
+  ru: {
+    translation: translationRU,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'ru',
+  fallbackLng: 'ru',
+  keySeparator: '.',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,29 +1,31 @@
+import { useTranslation } from 'react-i18next';
+
 import { GradientButton } from 'components/atoms/GradientButton';
 
 import styles from './styles.module.scss';
 
 export default () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <div className={styles['top-block']}>
-        <h1 className={styles.title}>Зарабатывай с FlowTok</h1>
-        <p className={styles.description}>
-          Создавай потрясающий контент и получай за него деньги
-        </p>
-        <GradientButton>Создать аккаунт или войти</GradientButton>
+        <h1 className={styles.title}>{t('home.title')}</h1>
+        <p className={styles.description}>{t('home.description')}</p>
+        <GradientButton>{t('home.signin-button-text')}</GradientButton>
       </div>
       <div className={styles['bottom-block']}>
         <div className={styles['stat']}>
           <h3 className={styles['value']}>563 195</h3>
-          <p className={styles['label']}>Пользователей в FlowTok</p>
+          <p className={styles['label']}>{t('home.stats.users')}</p>
         </div>
         <div className={styles['stat']}>
           <h3 className={styles['value']}>28 159 789 ₽</h3>
-          <p className={styles['label']}>Всего выплачено</p>
+          <p className={styles['label']}>{t('home.stats.summary')}</p>
         </div>
         <div className={styles['stat']}>
           <h3 className={styles['value']}>1 578 ₽</h3>
-          <p className={styles['label']}>Средний доход в день</p>
+          <p className={styles['label']}>{t('home.stats.median')}</p>
         </div>
       </div>
     </>
