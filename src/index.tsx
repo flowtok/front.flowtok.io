62d7f32db9be7bb8b77e8e9193359752303545a5
YoungProducer <sashabezrukovownmail@gmail.com>
2021-04-12 13:51:10 +0300

    Added sass-loader.

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { App } from './App';
-// import './styles/normalize.scss';
+import './styles/normalize.scss';
 import store from './redux/store';
 
 ReactDOM.render(
