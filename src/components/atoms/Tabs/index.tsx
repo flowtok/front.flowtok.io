dd9b0bb72e320d1f6329dbe0986b5f316ea4525a
YoungProducer <sashabezrukovownmail@gmail.com>
2021-04-14 09:40:33 +0300

    Added tabs component.

---
+++ src/components/atoms/Tabs/index.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames';
+
+import styles from './styles.module.scss';
+
+export interface TabsProps {
+  list: string[];
+  selected: number;
+  onSelect: (index: number) => void;
+}
+
+export const Tabs = ({ list, selected, onSelect }: TabsProps) => {
+  return (
+    <div className={styles['tabs-wrapper']}>
+      <div
+        className={styles['tabs-inner-wrapper']}
+        style={
+          {
+            '--index': selected,
+            '--count': list.length,
+          } as any
+        }
+      >
+        {list.map((item, index) => (
+          <button
+            key={index}
+            className={classNames(styles['tab'], {
+              [styles['tab_selected']]: index === selected,
+            })}
+            onClick={() => onSelect(index)}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
