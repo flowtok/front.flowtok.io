088bf91ef6830a453180d1967617277199c8e977
Robert Green <mrrobertgreen17@gmail.com>
2021-04-15 11:04:31 +0300

    fix switch component

--- src/components/atoms/Switch/index.tsx
+++ src/components/atoms/Switch/index.tsx
@@ -1,36 +1,26 @@
-import React, { FC, HTMLProps, useState } from 'react';
+import React, { FC, HTMLProps } from 'react';
 import styles from './styles.module.scss';
 
 type SwitchPropsT = {
-  hasLabel: boolean;
+  label?: string;
 };
 
 export const Switch: FC<SwitchPropsT & HTMLProps<HTMLInputElement>> = ({
-  hasLabel,
+  label,
   ...rest
 }) => {
-  const [isBonus, setIsBonus] = useState(hasLabel);
-
-  const onSwitch = () => {
-    if (hasLabel) {
-      setIsBonus((prev) => !prev);
-    }
-  };
-
   return (
     <div className={styles['switch-wrapper']}>
       <input
         className={styles['switch-checkbox']}
         type="checkbox"
         id="switch"
-        defaultChecked={isBonus}
-        onChange={onSwitch}
         {...rest}
       />
-      <label className={styles['switch-track']} htmlFor="switch">
+      <label className={styles['switch-track']} htmlFor={'switch'}>
         <span className={styles['switch-circle']} />
       </label>
-      {isBonus && hasLabel && <div className={styles['label']}>+1.00 ₽</div>}
+      {label && <div className={styles['label']}>{label}</div>}
     </div>
   );
 };
