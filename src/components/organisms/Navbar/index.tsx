94145b405fd18d3725fd150f43179a46a5b77753
Robert Green <mrrobertgreen17@gmail.com>
2021-05-04 16:26:17 +0300

    finish navbar

--- src/components/organisms/Navbar/index.tsx
+++ src/components/organisms/Navbar/index.tsx
@@ -1,26 +1,29 @@
-import React, { forwardRef } from 'react';
-import { Link } from 'react-router-dom';
+import React, { forwardRef, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './styles.module.scss';
 import { ProfileIcon } from './Icons/ProfileIcon';
 import { TasksIcon } from './Icons/TasksIcon';
 import { SettingsIcon } from './Icons/SettingsIcon';
 import { useTranslation } from 'react-i18next';
+import cn from 'classnames';
 
 export const Navbar = forwardRef<HTMLDivElement>(({}, ref) => {
   const { t } = useTranslation();
+  const { pathname } = useLocation();
+
   return (
     <div className={styles['wrapper']} ref={ref}>
       <div className={styles['row']}>
         <Link to="/profile" className={styles['row-item']}>
-          <ProfileIcon />
+          <ProfileIcon isActive={pathname === '/profile'} />
           <p className={styles['label']}>{t('header.profile.title')}</p>
         </Link>
         <Link to="/tasks" className={styles['row-item']}>
-          <TasksIcon />
+          <TasksIcon isActive={pathname === '/tasks'} />
           <p className={styles['label']}>{t('header.tasks.title')}</p>
         </Link>
         <Link to="/settings" className={styles['row-item']}>
-          <SettingsIcon />
+          <SettingsIcon isActive={pathname === '/settings'} />
           <p className={styles['label']}>{t('header.settings.title')}</p>
         </Link>
       </div>
