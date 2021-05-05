dddc561c50cdfb1ee2b5538552fc7084d3b52c23
Иван Статкевич <statkevich.ivan999@gmail.com>
2021-05-05 09:13:53 +0300

    finished working on "tasks" popup

---
+++ src/components/molecules/TaskCard/TasksPopUp/index.tsx
@@ -0,0 +1,48 @@
+import { forwardRef, PropsWithChildren } from 'react';
+import styles from './styles.module.scss';
+import { PopUp } from '../../PopUp';
+import { useTranslation } from 'react-i18next';
+
+export enum TasksPopUpTypes {
+  thatIsDone = 'thatDone',
+  howToDo = 'todo',
+}
+
+interface TasksPopUpProps {
+  isOpen: boolean;
+  close: () => void;
+  type: string;
+  tasks: { value: string; isDone: boolean }[];
+}
+
+export const TasksPopUp = forwardRef<
+  HTMLDivElement,
+  PropsWithChildren<TasksPopUpProps>
+>(({ isOpen, close, type, tasks }) => {
+  const { t } = useTranslation();
+  let title = t('task-cards.popup-titles.how-to-do');
+
+  if (type === TasksPopUpTypes.thatIsDone && tasks.find((t) => t.isDone)) {
+    title = t('task-cards.popup-titles.that-is-done');
+  }
+  if (type === TasksPopUpTypes.thatIsDone && tasks.find((t) => !t.isDone)) {
+    title = t('task-cards.popup-titles.that-is-not-done');
+  }
+
+  return (
+    <PopUp isOpen={isOpen} close={close} title={title}>
+      <ul className={styles['task-list']}>
+        {tasks.map((task, key) => {
+          if (type === TasksPopUpTypes.thatIsDone && task.isDone) {
+            return (
+              <li key={'task' + key} style={{ textDecoration: 'line-through' }}>
+                {task.value}
+              </li>
+            );
+          }
+          return <li key={'task' + key}>{task.value}</li>;
+        })}
+      </ul>
+    </PopUp>
+  );
+});
