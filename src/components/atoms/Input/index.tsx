005b49ed7c36f9408cbc363d8ac245cbdbf3ef0e
Иван Статкевич <statkevich.ivan999@gmail.com>
2021-05-06 22:51:44 +0300

    finished working on masks and validation for payment methods, added react-mask-js to dependencies

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,5 +1,5 @@
 import { DetailedHTMLProps, forwardRef, InputHTMLAttributes } from 'react';
-
+import InputMask from 'react-input-mask';
 import styles from './styles.module.scss';
 import classNames from 'classnames';
 import { FieldError, UseFormRegister } from 'react-hook-form';
@@ -8,12 +8,13 @@ export type InputProps = {
   error?: FieldError;
   visited?: boolean;
   placeholder?: string;
+  mask?: string | Array<string | RegExp>;
 } & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 export const Input = forwardRef<
   HTMLInputElement,
   InputProps & ReturnType<UseFormRegister<any>>
->(({ error, visited, placeholder, ...rest }, ref) => {
+>(({ error, visited, placeholder, mask = '', ...rest }, ref) => {
   const finalClassName = classNames(styles['input'], {
     [styles[`input-error`]]: !!error,
     [styles[`input-visited`]]: visited,
@@ -21,12 +22,9 @@ export const Input = forwardRef<
 
   return (
     <div>
-      <input
-        className={finalClassName}
-        {...rest}
-        ref={ref}
-        placeholder={placeholder}
-      />
+      <InputMask mask={mask} {...rest}>
+        {() => <input {...rest} className={finalClassName} />}
+      </InputMask>
       {error && <p className={styles['error']}>{error.message}</p>}
     </div>
   );
