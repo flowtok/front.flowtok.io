2bbdda7c424a89364f03a2534ea123bc3763018f
Иван Статкевич <statkevich.ivan999@gmail.com>
2021-05-16 21:40:00 +0300

    finished working on adaptive for settings page(1920)

--- src/components/molecules/SettingsCards/Support/index.tsx
+++ src/components/molecules/SettingsCards/Support/index.tsx
@@ -10,32 +10,34 @@ export const SupportCard = () => {
 
   return (
     <Paper>
-      <h3 className={commonStyles['primary-title']}>
-        {t('pages.settings.cards.withdrawal.support-title')}
-      </h3>
-      <div className={styles['saved-block']}>
-        <p className={commonStyles['description']}>
-          {t('pages.settings.cards.withdrawal.support-helper-text')}
-        </p>
-        <div className={styles['support-methods']}>
-          <div className={styles['support-method']}>
-            <p>{t('pages.settings.cards.withdrawal.support-methods.post')}</p>
-            <CustomLink
-              iSTargetBlank={true}
-              url="flowtokcom@gmail.com"
-              value="flowtokcom@gmail.com"
-            />
-          </div>
-          <Divider />
-          <div className={styles['support-method']}>
-            <p>
-              {t('pages.settings.cards.withdrawal.support-methods.telegram')}
-            </p>
-            <CustomLink
-              iSTargetBlank={true}
-              url="@flowtokcom"
-              value="@flowtokcom"
-            />
+      <div className={styles['support-container']}>
+        <h3 className={commonStyles['primary-title']}>
+          {t('pages.settings.cards.withdrawal.support-title')}
+        </h3>
+        <div className={styles['saved-block']}>
+          <p className={commonStyles['description']}>
+            {t('pages.settings.cards.withdrawal.support-helper-text')}
+          </p>
+          <div className={styles['support-methods']}>
+            <div className={styles['support-method']}>
+              <p>{t('pages.settings.cards.withdrawal.support-methods.post')}</p>
+              <CustomLink
+                iSTargetBlank={true}
+                url="flowtokcom@gmail.com"
+                value="flowtokcom@gmail.com"
+              />
+            </div>
+            <Divider />
+            <div className={styles['support-method']}>
+              <p>
+                {t('pages.settings.cards.withdrawal.support-methods.telegram')}
+              </p>
+              <CustomLink
+                iSTargetBlank={true}
+                url="@flowtokcom"
+                value="@flowtokcom"
+              />
+            </div>
           </div>
         </div>
       </div>
