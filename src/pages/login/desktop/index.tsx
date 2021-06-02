e60effaa70aef94b9d5808a8ac05a7ad17ceedb3
Robert Green <mrrobertgreen17@gmail.com>
2021-06-02 09:34:36 +0300

    fixes

--- src/pages/login/desktop/index.tsx
+++ src/pages/login/desktop/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import videoMp4 from '../../../assets/login/videos/login_video.mp4';
 import videoOgv from '../../../assets/login/videos/login_video.ogv';
 import videoWebm from '../../../assets/login/videos/login_video.webm';
-import { AuthenticationTemplate } from '../../../components/templates/SignUpDesktopTempate';
+import { AuthenticationTemplate } from '../../../components/templates/AuthenticationTemplate';
 import { LoginForm } from '../../../components/molecules/Login/LoginForm';
 
 export default () => {
