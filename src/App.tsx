482c919a5f70d526252ae37e0bba894584431f77
Иван Статкевич <statkevich.ivan999@gmail.com>
2021-06-11 13:08:15 +0300

    working with apollo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React, { Suspense } from 'react';
 import { AuthLayout } from './components/templates/AuthLayout';
 import { NonAuthLayout } from './components/templates/NonAuthLayout';
 import { isAuthVar } from './api/cache';
+import { useReactiveVar } from '@apollo/client';
 
 export const App = () => {
+  const isAuth = useReactiveVar(isAuthVar);
+
   return (
     <div style={{ position: 'relative' }}>
       <Suspense fallback={<div>Loading...</div>}>
-        {isAuthVar() ? <AuthLayout /> : <NonAuthLayout />}
+        {!isAuth ? <AuthLayout /> : <NonAuthLayout />}
       </Suspense>
     </div>
   );
--- src/api/cache.ts
+++ src/api/cache.ts
@@ -4,4 +4,16 @@ import { User } from '../models/models';
 export const currentUserVar = makeVar<User | null>(null);
 export const isAuthVar = makeVar<boolean>(!!localStorage.getItem('token'));
 
-export const cache = new InMemoryCache();
+export const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        isAuth: {
+          read() {
+            return isAuthVar();
+          },
+        },
+      },
+    },
+  },
+});
